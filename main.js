72d0180386e5bd2e3005666d0caf0eadc0256f89
Maximilian Kranabetter <maximilian.kranabetter@student.uibk.ac.at>
2018-04-11 17:27:36 +0200

    first steps in javascript

---
+++ main.js
@@ -0,0 +1,32 @@
+// alert("Ok?");
+
+let nachricht = "Hallo Welt";
+// alert(nachricht);
+nachricht = "neuer Text";
+// alert(nachricht);
+console.log(nachricht);
+const linkColor = '#ff0000';
+let eine_variable = 'test';
+console.log("Variable: eine_variable: ", eine_variable);
+let highScore = 925000;
+console.log("Ihr Kontostand ist: ", highScore*highScore*highScore*(-1),"€");
+let meineNachricht = "test1";
+console.log(meineNachricht);
+meineNachricht = "test2";
+console.log(meineNachricht);
+meineNachricht = `Ein mehr
+und ein
+ganz langer
+text
+und aus`;
+
+console.log(meineNachricht);
+
+let fullName = 'Jeffrey "The Dude" Lebowski';
+console.log(fullName);
+
+let isOver18 = true;
+console.log("über 18? Sonst: du kummsch da net rein.", isOver18);
+let age = 17;
+console.log("age ist:", age)
+console.log("age über 18", age > 18);
